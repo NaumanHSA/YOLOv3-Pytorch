937c22c5017cf191c1fdeec276bd4be7bfb244ca
NaumanHSA <Naumanhsa965@gmail.com>
2019-07-20 12:57:35 +0500

    Update image_download.py

--- WebImage_Scraping/image_download.py
+++ WebImage_Scraping/image_download.py
@@ -2,7 +2,7 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"weapon,knife,pistol","limit":100,"chromedriver":"C:\\Users\\nauma\\OneDrive\\Desktop\\yolov3workflow-master\\1_WebImage_Scraping\\chromedriver.exe","print_urls":True}   #creating list of arguments
+arguments = {"keywords":"<keywords>","limit":100,"chromedriver":"<chromedriver path>","print_urls":True}   #creating list of arguments
 #Make sure to set chromedrive is the appropriate director if you want to download more than 100 images
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
