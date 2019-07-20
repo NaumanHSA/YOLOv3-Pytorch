eb48eaa01ceb9845e128d773d975650e4899ce33
NaumanHSA <Naumanhsa965@gmail.com>
2019-07-20 16:51:47 +0500

    Delete image_download.py

--- WebImage_Scraping/image_download.py
+++
@@ -1,8 +0,0 @@
-from google_images_download import google_images_download   #importing the library
-
-response = google_images_download.googleimagesdownload()   #class instantiation
-
-arguments = {"keywords":"<keywords>","limit":100,"chromedriver":"<chromedriver path>","print_urls":True}   #creating list of arguments
-#Make sure to set chromedrive is the appropriate director if you want to download more than 100 images
-paths = response.download(arguments)   #passing the arguments to the function
-print(paths)   #printing absolute paths of the downloaded images
